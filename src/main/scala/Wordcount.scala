0ef8f06b21c703c0589d8fa699c2fc37a1295485
siddhusalvi <siddheshsalvi525@gmail.com>
2020-04-06 19:10:13 +0530

    REFACTORED: command line arguments

---
+++ src/main/scala/Wordcount.scala
@@ -0,0 +1,29 @@
+import org.apache.spark.{SparkConf, SparkContext}
+
+object Wordcount {
+
+  def main(args: Array[String]): Unit = {
+
+    val conf = new SparkConf().setAppName("s3wordcounter").setMaster("local")
+    val sc = new SparkContext(conf)
+
+    val bucket = args(0)
+    val file = args(1)
+
+    val key =
+    val secretkey =
+
+    System.setProperty("com.amazonaws.services.s3.enableV4", "true")
+    sc.hadoopConfiguration.set("fs.s3a.access.key", key)
+    sc.hadoopConfiguration.set("fs.s3a.secret.key", secretkey)
+    sc.hadoopConfiguration.set("fs.s3a.endpoint", "s3.us-east-2.amazonaws.com")
+    sc.hadoopConfiguration.set("fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem")
+
+    val text = sc.textFile("s3a://" + bucket + "/" + file)
+    val words = text.flatMap(line => line.split(","))
+    val pairs = words.map(word => (word, 1))
+    val counts = pairs.reduceByKey(_ + _).sortByKey()
+    counts.foreach(println(_))
+
+  }
+}
